642b14bd8fe5dabb92d51667aa9d8f18dca99430
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-11-17 21:47:26 +0000

    added about page, added categories below admin tab

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+  #ABOUT US page 
+  get 'about' => 'about#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
