6a7b03c1d8b054bfb1140b972b484ef04c324c5a
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-11-18 03:26:40 +0000

    user authorization, and login functionality implemented, login/logout implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  # action to send someone to the login page if they are not logged in, commented out for now since we are not implementing this feature until later in the project 
+  # def authorize
+  #   redirect_to '/login' unless current_user
+  # end
+
+private
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
