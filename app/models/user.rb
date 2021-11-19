9c7a28fc0682d0c3a58a1246e90f94106db56222
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-11-19 04:11:27 +0000

    rspec for user added and implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,26 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :password, presence: true, length: {minimum: 5}
+  validates :password_confirmation, presence: true, length: {minimum: 5}
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
+  #  Create an class method authenticate_with_credentials
+  def self.authenticate_with_credentials(email_address, password)
+
+    # see if there is a user in the database with the given email
+    user = User.find_by_email(email_address.strip.downcase)
+
+    # If the user exists AND the password provided is correct then return and instance of that user, if not return nil
+
+    if user && user.authenticate(password)
+      return user
+    else
+      nil
+    end
 end 
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,119 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    it "should save when all required fields are filled" do 
+      @user = User.new({password:'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.save 
+      expect(@user).to be_present
+    end 
+
+    it "returns 'First name can't be blank' if we try to create a User with a nil value in the first name field"  do 
+      @user = User.new({password:'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: nil, last_name: "Brothers" })
+
+      @user.valid?
+
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+
+    end 
+
+    it "should display Last name can't be blank!" do 
+      @user = User.new({password:'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: nil })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end 
+
+    it "should display email can't be blank!" do 
+      @user = User.new({password:'nintendo', password_confirmation: 'nintendo', email: nil, first_name: "Mario", last_name: "Brothers" })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end 
+
+    it "should display password can't be blank!" do 
+      @user = User.new({password:nil, password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end 
+
+    it "should display password can't be less than 5 characters!" do 
+      @user = User.new({password: "hi", password_confirmation: "hi", email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end 
+
+    it "should display password confirmation can't be less than 5 characters!" do 
+      @user = User.new({password: "hi", password_confirmation: "hi", email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end 
+
+
+    it "should display password confirmation can't be less than 5 characters!" do 
+      @user = User.new({password: "hi", password_confirmation: "hi", email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end 
+
+
+    it "should display password does not match the password confirmation" do 
+      @user = User.new({password: "nintendo", password_confirmation: "hi", email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end 
+
+  end 
+
+
+  describe '.authenticate_with_credentials' do
+    #create a user and then test if they can login
+    it "returns an instance of the user model if an existing user tries to login with the correct email and password" do
+      @user = User.new({password: 'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.save
+
+      expect(User.authenticate_with_credentials('nintendo@gmail.com', 'nintendo' )).to eq(@user)
+    end
+
+
+    it "returns nil if an existing user tries to login with the correct email and incorrect password" do
+      @user = User.new({password: 'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.save
+
+      expect(User.authenticate_with_credentials('nintendo@gmail.com', 'hi' )).to be nil
+    end
+
+    it "returns nil if user attempts to login with an email that is not in the database " do
+      @user = User.new({password: 'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.save
+
+      expect(User.authenticate_with_credentials('atari@gmail.com', 'nintendo' )).to be nil
+    end
+
+    it "returns an instance of the user model if an existing user tries to login with the correct email(with inconsistences in the case of the characters) and password"do
+      @user = User.new({password: 'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+      @user.save
+
+      expect(User.authenticate_with_credentials('NinTendo@gmail.com','nintendo')).to eq(@user)
+    end
+
+    it  "returns an instance of the user model if an existing user tries to login with the correct email(with some extra whitespace characters added to the start of the input) and correct password" do
+    @user = User.new({password: 'nintendo', password_confirmation: 'nintendo', email: 'nintendo@gmail.com', first_name: "Mario", last_name: "Brothers" })
+
+    @user.save
+
+    expect(User.authenticate_with_credentials('    nintendo@gmail.com', 'nintendo')).to eq(@user)
+  end
+end
+end
